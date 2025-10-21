9578a5bee384daf9711fb7443c6d77babad4fc2a
Tóth Balázs <tothfazekasbalazs@gmail.com>
2025-10-21 14:15:46 +0200

    deploy on github

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env sh
+set -e
+npm run build
+cd dist
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f git@github.com: pragmatic-reviews/esoerdo_miniprojekt.git main:gh-pages
+cd -
